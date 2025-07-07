be90919a904f57bed7812ce8b9ea2150e78d9cb8
kawsarkabir <devkawsarkabir@gmail.com>
2025-07-08 03:12:30 +0600

    chore(config): initially project setup and connected with mongoDB

---
+++ src/middleware/verifyJWT.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const verifyJWT = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Unauthorized access" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ message: "Forbidden access" });
+
+    req.user = decoded;
+    next();
+  });
+};
+
+export default verifyJWT;
