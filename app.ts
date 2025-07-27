44fc380ac5ca9645877acbad61756bc212c05adb
kawsarkabir <devkawsarkabir@gmail.com>
2025-07-28 03:14:27 +0600

    feat(mvc): convert mvc pettern and added typescript

--- app.ts
+++ app.ts
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://grand-royel.vercel.app", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 const apiVersion = process.env.API_VERSION;
 
--- dist/config/firebase.js
+++ dist/config/firebase.js
@@ -1,4 +1,6 @@
 import admin from "firebase-admin";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 const serviceAccount = require("../../firebase-adminsdk.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
