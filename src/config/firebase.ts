23bf6b81e9e2ce5c58309cddec8868bb31f1e464
kawsarkabir <devkawsarkabir@gmail.com>
2025-08-10 17:51:49 +0600

    fix(firebase config): firebase config convert json to 64base

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,37 +1,17 @@
-// import admin from "firebase-admin";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const fileData = Buffer.from(
-//   process.env.FIREBASE_SERVICES_KEY,
-//   "base64"
-// ).toString("utf8");
-
-// const serviceAccount = JSON.parse(fileData);
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-// export default admin;
-
-// firebase.ts
 import admin from "firebase-admin";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
-// Get __dirname equivalent in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const fileData = Buffer.from(
+  process.env.FIREBASE_SERVICES_KEY,
+  "base64"
+).toString("utf8");
 
-const serviceAccountPath = path.join(__dirname, "../../firebase-adminsdk.json");
+const serviceAccount = JSON.parse(fileData);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountPath),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
